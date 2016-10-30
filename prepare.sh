c7106cf4e7fc6474304c8ad62d1ed2366d8f3d9f
Daniel Ta <daniel.md.ta@outlook.com>
2016-10-30 20:54:12 +0200

    Update plots

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,7 @@ trap "set +x; sleep 2; set -x" DEBUG
 rm -rf /usr/local/bin/mn /usr/local/bin/mnexec /usr/local/lib/python*/*/*mininet* /usr/local/bin/ovs-* /usr/local/sbin/ovs-*
 apt-get install -y python-setuptools
 sudo easy_install pip
+pip install -r requirements.txt
 apt-get install -y openvswitch-testcontroller && cp /usr/bin/ovs-testcontroller /usr/bin/ovs-controller
 modprobe tcp_vegas
 modprobe tcp_probe full=1
--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 trap "set +x; sleep 5; set -x" DEBUG
 
-python bin/run.py -bw 0.01 -d 500 -l 0 -a reno
-python bin/run.py -bw 2 -d 500 -l 0 -a reno
-python bin/run.py -bw 2 -d 2 -l 5 -a reno
-python bin/run.py -bw 0.01 -d 500 -l 0 -a cubic
-python bin/run.py -bw 2 -d 500 -l 0 -a cubic
-python bin/run.py -bw 2 -d 2 -l 5 -a cubic
-python bin/run.py -bw 0.01 -d 500 -l 0 -a vegas
-python bin/run.py -bw 2 -d 500 -l 0 -a vegas
-python bin/run.py -bw 2 -d 2 -l 5 -a vegas
-sleep 3 && echo DONE!!
+python bin/run.py -bw 0.01 -d 2 -l 0 -a reno
+python bin/run.py -bw 2 -d 2 -l 0 -a reno
+python bin/run.py -bw 2 -d 500 -l 5 -a reno
+python bin/run.py -bw 0.01 -d 2 -l 0 -a cubic
+python bin/run.py -bw 2 -d 2 -l 0 -a cubic
+python bin/run.py -bw 2 -d 500 -l 5 -a cubic
+python bin/run.py -bw 0.01 -d 2 -l 0 -a vegas
+python bin/run.py -bw 2 -d 2 -l 0 -a vegas
+python bin/run.py -bw 2 -d 500 -l 5 -a vegas
+sleep 5 && echo DONE!!
